9f13e734c02fb99c869ecca8ff54ea305891e52a
Bernhard Boser <bernhard.e.boser@gmail.com>
2026-02-20 10:33:11 +0100

    Improve broken link reporting

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,1 +1,35 @@
-../../blog-content/navigation.ts
+import { getPermalink, getBlogPermalink } from '~/utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
+    },
+    {
+      text: 'Railroad',
+      href: getPermalink('railroad', 'tag'),
+    },
+    {
+      text: 'Docs',
+      href: getPermalink('doc', 'tag'),
+    },
+    {
+      text: 'About',
+      href: getPermalink('/about'),
+    },
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  links: [],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [],
+  footNote: `
+    Boser-Guyon · All rights reserved.
+  `,
+};
